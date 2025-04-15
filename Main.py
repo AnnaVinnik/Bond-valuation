af20469ff0bab2db27f66f68b8a7c6ad3da8a7ee
Anna <vinnik.anna@rambler.ru>
2025-04-15 13:17:00 +0200

    Add calculate_pv_annuity function to compute present value of an annuity

--- Main.py
+++ Main.py
@@ -6,7 +6,13 @@ def calculate_ear(apr, n):
     print (round(ear, 4))
     return ear
 
+def calculate_pv_annuity(C, r, t):
+    pv = C / r * (1 - 1 / math.pow(1 + r, t))
+
+    return pv
+
 apr = 0.15
 n = 12
 
 calculate_ear(apr, n)
+print(round(calculate_pv_annuity(1000, 0.01, 3), 4))
